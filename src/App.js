2dc9f5e23cbc34152e17a9c6754ef84164d612c7
Alexis <alexis.jaunet@etu.univ-nantes.fr>
2018-02-26 14:00:03 +0100

    Réadaptation de la page accueil

--- src/App.js
+++ src/App.js
@@ -1,35 +1,118 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import './App.css';
 import './header/Header.css';
-import './accueil/SearchHomepage.css';
-import './accueil/results/Result.css';
-import './accueil/results/ResultSet.css';
 
-var Header = require("./header/Header");
-var SearchHomepage = require("./accueil/SearchHomepage");
-var ResultSet = require("./accueil/results/ResultSet");
+var Header = require('./header/Header');
 
-class App extends Component {
-    constructor (props) {
+const Movie = props => (
+    <div className="movie">
+        <figure className="movie__figure">
+            <img src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${props.poster_path}`} className="movie__poster" />
+            <h2 className="movie__title">{props.title}</h2>
+        </figure>
+    </div>
+);
+
+Movie.propTypes = {
+    id         : PropTypes.number.isRequired,
+    title      : PropTypes.string.isRequired,
+    poster_path: PropTypes.string,
+};
+
+const Movies = props => (
+    <ul className="movies">
+        {props.movies.map(movie => (
+            <li key={movie.id}>
+                <Movie {...movie} />
+            </li>
+        ))}
+    </ul>
+);
+
+Movies.propTypes = {
+    movies: PropTypes.arrayOf(PropTypes.object)
+};
+
+const Search = props => (
+    <form className="search" onInput={event => props.onInput(event.target.value)}>
+        <input type="search" value={props.query} placeholder={props.placeholder} />
+    </form>
+);
+
+Search.propTypes = {
+    query      : PropTypes.string.isRequired,
+    onInput    : PropTypes.func.isRequired,
+    placeholder: PropTypes.string
+};
+
+class App extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = { token:"" };
 
-        this.refreshToken = this.refreshToken.bind(this);
+        this.state = {
+            movies: [],
+            query: ''
+        };
+
+        this.onInput = this.onInput.bind(this);
     }
 
-    refreshToken () {
-        this.setState({ token: sessionStorage.token });
+    onInput(query) {
+        this.setState({
+            query
+        });
+
+        if (query == '') {
+            this.getPopularMovies();
+        } else {
+            this.searchMovie(query);
+        }
+    }
+
+    getPopularMovies() {
+        const url = `https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716`;
+
+        fetch (url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    movies: data.results
+                })
+            });
+    }
+
+    searchMovie(query) {
+        const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=cfe422613b250f702980a3bbf9e90716`;
+
+        fetch (url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    movies: data.results
+                })
+            });
+    }
+
+    componentDidMount() {
+        this.getPopularMovies();
     }
 
     render() {
-    return (
-      <div className="App">
-          <Header />
-          <SearchHomepage/>
-          <ResultSet/>
-      </div>
-    );
-  }
+        const { movies, query } = this.state;
+        const isSearched = query => item => !query || item.title.toLowerCase().includes(query.toLowerCase());
+
+        return (
+            <div>
+                <Header />
+                <div className="app">
+                    <Search query={query} onInput={this.onInput} placeholder="Search for Movie…" />
+                    <Movies movies={movies.filter(isSearched(query))} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
