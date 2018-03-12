be2f52dcdb9a15ad69a2ec20691f9cd81dc2129a
Alexis <alexis.jaunet@etu.univ-nantes.fr>
2018-03-12 13:22:17 +0100

    Gestion des champs obligatoires

--- src/pages/Inscription.js
+++ src/pages/Inscription.js
@@ -4,7 +4,9 @@ var ReactDOM = require('react-dom');
 class Inscription extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {pseudo: '', mdp: '', email: '', ville: '', adresse: '', page: props.page};
+        this.state = {infos: {pseudo: '', mdp: '', email: '', ville: '', adresse: ''},
+                      obligations: {pseudo: false, mdp: false, ville:false}, page: props.page,
+                      errors: {pseudo:"", mdp:"", ville:""}};
 
         this.handlePseudo  = this.handlePseudo.bind(this);
         this.handleMdp     = this.handleMdp.bind(this);
@@ -17,10 +19,38 @@ class Inscription extends React.Component {
 
     handlePseudo(event) {
         this.setState({value: event.target.pseudo});
+
+        if (this.state.infos.pseudo != "") {
+            this.setState({
+                obligations: {
+                    pseudo: true
+                }
+            });
+        } else {
+            this.setState({
+                obligations: {
+                    pseudo: false
+                }
+            });
+        }
     }
 
     handleMdp(event) {
         this.setState({value: event.target.mdp});
+
+        if (this.state.infos.mdp != "") {
+            this.setState({
+                obligations: {
+                    mdp: true
+                }
+            });
+        } else {
+            this.setState({
+                obligations: {
+                    mdp: false
+                }
+            });
+        }
     }
 
     handleEmail(event) {
@@ -29,6 +59,20 @@ class Inscription extends React.Component {
 
     handleVille(event) {
         this.setState({value: event.target.ville});
+
+        if (this.state.infos.ville != "") {
+            this.setState({
+                obligations: {
+                    ville: true
+                }
+            });
+        } else {
+            this.setState({
+                obligations: {
+                    ville: false
+                }
+            });
+        }
     }
 
     handleAdresse(event) {
@@ -36,8 +80,35 @@ class Inscription extends React.Component {
     }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
         event.preventDefault();
+
+        if (! this.state.obligations.pseudo) {
+            this.setState ({
+                errors: {
+                    pseudo: "Erreur : Pseudo manquant"
+                }
+            })
+        }
+
+        if (! this.state.obligations.mdp) {
+            this.setState ({
+                errors: {
+                    mdp: "Erreur : Mot de passe manquant"
+                }
+            })
+        }
+
+        if (! this.state.obligations.ville) {
+            this.setState ({
+                errors: {
+                    ville: "Erreur : Ville manquante"
+                }
+            })
+        }
+
+        if (this.state.obligations.pseudo && this.state.obligations.mdp && this.state.obligations.ville) {
+            // envoi inscription
+        }
     }
 
     goToConnexion () {
@@ -51,15 +122,15 @@ class Inscription extends React.Component {
                     <div className="login-html">
                         <input id="tab-1" type="radio" name="tab" className="sign-in"/><label className="tab change" onClick={this.goToConnexion}>Connexion</label>
                         <input id="tab-2" type="radio" name="tab" className="sign-up" checked/><label htmlFor="tab-2" className="tab">Inscription</label>
-                        <div className="login-form">
+                        <form className="login-form">
                             <div className="sign-up-htm">
                                 <div className="group">
                                     <label htmlFor="user" className="label">Pseudo *</label>
-                                    <input id="user" type="text" className="input"/>
+                                    <input id="user" type="text" className="input" required/>
                                 </div>
                                 <div className="group">
                                     <label htmlFor="pass" className="label">Mot de passe *</label>
-                                    <input id="pass" type="password" className="input" data-type="password"/>
+                                    <input id="pass" type="password" className="input" data-type="password" required/>
                                 </div>
                                 <div className="group">
                                     <label htmlFor="pass" className="label">E-mail</label>
@@ -67,17 +138,22 @@ class Inscription extends React.Component {
                                 </div>
                                 <div className="group">
                                     <label htmlFor="pass" className="label">Ville *</label>
-                                    <input id="pass" type="text" className="input"/>
+                                    <input id="pass" type="text" className="input" required/>
                                 </div>
                                 <div className="group">
                                     <label htmlFor="pass" className="label">Adresse</label>
                                     <input id="pass" type="text" className="input"/>
                                 </div>
                                 <div className="group">
-                                    <input type="submit" className="button" value="Go !"/>
+                                    <label className="error">{this.state.errors.pseudo}</label>
+                                    <label className="error">{this.state.errors.mdp}</label>
+                                    <label className="error">{this.state.errors.ville}</label>
+                                </div>
+                                <div className="group">
+                                    <input type="submit" className="button" value="Go !" onSubmit={this.handleSubmit}/>
                                 </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
